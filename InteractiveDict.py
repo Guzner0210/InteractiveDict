c626ba8a1f0e177c3bfcda29bb9e3e6e982e2808
Guzner0210 <43829885+Guzner0210@users.noreply.github.com>
2018-11-27 16:55:04 -0500

    Update InteractiveDict.py
    Working on some widgets...

--- InteractiveDict.py
+++ InteractiveDict.py
@@ -2,10 +2,10 @@ from tkinter import *
 from tkinter.ttk import *
 import json
 import sqlite3
-from sqlite3 import Error
+from difflib import get_close_matches
 
 root = Tk()
-root.geometry("500x300")
+root.geometry("500x100")
 
 #============================
 def trans():
@@ -18,10 +18,9 @@ def trans():
         return dict[search]
 
 
-    answer = Label(root, text=translate(search)[0])
-    answer.grid(row=1, column=1)
-
     if search in dict.keys():
+        answer = Label(root, text=translate(search)[0] + "|")
+        answer.grid(row=2, column=1, columnspan="2", sticky="w")
         db = sqlite3.connect("db.sqlite3")
         cursor = db.cursor()
         cursor.execute(
@@ -31,7 +30,15 @@ def trans():
         db.commit()
         db.close()
     else:
-        print("nope")
+        close_match = get_close_matches(search, dict.keys())
+        suggestion_1 = Button(root, text=close_match[0])
+        suggestion_1.grid(row=1, column=0, sticky="w")
+        suggestion_2 = Button(root, text=close_match[1])
+        suggestion_2.grid(row=1, column=1, sticky="w")
+        suggestion_3 = Button(root, text=close_match[2])
+        suggestion_3.grid(row=1, column=2, sticky="w")
+        # answer = Label(root, text="That word is not in this dictionary, sorry!|")
+        # answer.grid(row=1, column=1, sticky="w")
 
 #============================
 
@@ -45,6 +52,6 @@ search_btn = Button(root, text="Search", command=trans)
 search_btn.grid(row=0, column=2, padx=10)
 
 label_2 = Label(root, text="Your word's meaning: ")
-label_2.grid(row=1, column=0)
+label_2.grid(row=2, column=0)
 
 root.mainloop()
